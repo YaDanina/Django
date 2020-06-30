4ac43022dc2e34b3c41b188e877104e4847649fb
yadanina <krasavin777@me.com>
2020-06-30 11:26:46 +0300

    modified:   db.sqlite3        modified:   mysite/__pycache__/__init__.cpython-38.pyc 	modified:   mysite/__pycache__/settings.cpython-38.pyc 	modified:   mysite/__pycache__/urls.cpython-38.pyc 	modified:   mysite/__pycache__/wsgi.cpython-38.pyc 	modified:   mysite/apps/dev/SETTING.py 	modified:   mysite/apps/dev/__pycache__/BGInfo.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/SETTING.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/__init__.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/admin.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/apps.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/choices.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/filters.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/forms.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/models.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/urls.cpython-38.pyc 	modified:   mysite/apps/dev/__pycache__/views.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/0001_initial.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/0001_squashed_0001_initial.cpython-38.pyc modified:   mysite/apps/dev/migrations/__pycache__/0002_hardware_test.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/0003_printers.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/0004_auto_20200531_1738.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/0005_auto_20200623_1357.cpython-38.pyc 	modified:   mysite/apps/dev/migrations/__pycache__/__init__.cpython-38.pyc 	modified:   mysite/apps/dev/static/dev/js/index.js 	new file:   mysite/apps/dev/static/dev/slaider.css 	modified:   mysite/apps/dev/static/dev/style.css 	modified:   mysite/apps/printer/__pycache__/__init__.cpython-38.pyc 	modified:   mysite/apps/printer/__pycache__/admin.cpython-38.pyc 	modified:   mysite/apps/printer/__pycache__/apps.cpython-38.pyc 	modified:   mysite/apps/printer/__pycache__/models.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0001_initial.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0002_remove_printers_stat_type_printers.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0003_printers_stat_cartridge_type.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0004_auto_20200623_1650.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0005_auto_20200623_1651.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0006_auto_20200623_1652.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0007_auto_20200625_0931.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0008_printer_color.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/0009_auto_20200626_1509.cpython-38.pyc 	modified:   mysite/apps/printer/migrations/__pycache__/__init__.cpython-38.pyc 	modified:   mysite/templates/base.html 	modified:   mysite/templates/dev/main.html

--- mysite/apps/dev/SETTING.py
+++ mysite/apps/dev/SETTING.py
@@ -3,9 +3,9 @@ import os
 
 
 '''База данных SQLITE основная'''
-BD = os.path.join(r'C:\Django_git\Django\db.sqlite3')
-#BD = os.path.join(r'e:/Django/mysite/mysite/db.sqlite3')
+#BD = os.path.join(r'C:\Django_git\Django\db.sqlite3')
+BD = os.path.join(r'e:/Django/mysite/mysite/db.sqlite3')
 '''Подключение к базе данных BGInfo'''
-#CONN = connection_pyodbc()
+CONN = connection_pyodbc()
 '''Подключение к базе данных SQLITE'''
 CONN_DB = connection(BD)
--- mysite/apps/dev/static/dev/js/index.js
+++ mysite/apps/dev/static/dev/js/index.js
@@ -1,9 +1,69 @@
-let copy_btn = document.querySelectorAll('.copy_btn')
+let copy_btn = document.querySelectorAll('.copy_btn');
+let position = 0;
+const slaidesToShow = 3;
+const slaidesToScroll = 3;
+const container = document.querySelector('.flex-wrapper');
+const track = document.querySelector('.slaider-wrapper');
+const slaides = document.querySelectorAll('.slaider');
+const btnPrev = document.querySelector('.btnPrev');
+const btnNext = document.querySelector('.btnNext');
+const slaidesCount = slaides.length;
+const slaidesWidth = container.clientWidth / slaidesToShow;
+const movePosition = slaidesToScroll * slaidesWidth;
+
+
+slaides.forEach((slaide) => {
+    slaide.style.minwidth = `${slaidesWidth}px`
+})
+
+btnNext.addEventListener('click',() =>{
+    const slaidesLeft = slaidesCount - (Math.abs(position) + slaidesToShow * slaidesWidth) / slaidesWidth;
+    position -= slaidesLeft >= slaidesToScroll ? movePosition : slaidesLeft * slaidesWidth
+    setPosition();
+    checkBtn();
+})
+
+btnPrev.addEventListener('click',() =>{
+    const slaidesLeft = Math.abs(position) / slaidesWidth;
+    position += slaidesLeft >= slaidesToScroll ? movePosition : slaidesLeft * slaidesWidth
+    setPosition();
+    checkBtn();
+})
+
 for (let i of copy_btn){
     i.addEventListener('click', function(){
         let input = i.previousElementSibling
         input.select()
         document.execCommand('copy')
     })
-
 }
+
+const setPosition = () =>{
+    track.style.transform = `translateX(${position}px)`
+}
+
+const checkBtn = () =>{
+    btnPrev.disabled = position === 0;
+    btnPrev.disabled ? btnPrev.classList.add('btnDisable') : btnPrev.classList.remove('btnDisable');
+
+    btnNext.disabled = position === -(container.clientWidth * (slaidesCount/slaidesToShow) - container.clientWidth);
+    btnNext.disabled ? btnNext.classList.add('btnDisable') : btnNext.classList.remove('btnDisable');    
+}
+
+checkBtn();
+
+function replacePosition (){
+    if (position === 0 || position === -container.clientWidth){
+        const slaidesLeft = slaidesCount - (Math.abs(position) + slaidesToShow * slaidesWidth) / slaidesWidth;
+        position -= slaidesLeft >= slaidesToScroll ? movePosition : slaidesLeft * slaidesWidth;
+        setPosition();
+        checkBtn();
+    }else{
+        position = 0;
+        setPosition();
+        checkBtn();
+    }
+}
+
+setInterval(replacePosition, 6000);
+
